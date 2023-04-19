33c706ceee90a62f5de7a56d0cfbe312d764567c
Ouluhh <103758499+Ouluhh@users.noreply.github.com>
2023-04-19 17:39:33 +0200

    bugfixes

--- src/api.php
+++ src/api.php
@@ -15,14 +15,14 @@ class Api
     public function getUser()
     {
         $client = new Client([
-            'base_uri' => 'https://api.squlio.nl/v1',
+            'base_uri' => 'https://api.squlio.nl/',
             'headers' => [
                 'Authorization' => $this->apiKey,
                 'Accept' => 'application/json',
             ]
         ]);
 
-        $response = $client->get('/users');
+        $response = $client->get('/v1/users');
 
         return json_decode($response->getBody(), true);
     }
