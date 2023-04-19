442e910a78b89c64189de051f209a009f4580542
Ouluhh <103758499+Ouluhh@users.noreply.github.com>
2023-04-19 15:25:21 +0200

    test

---
+++ tests/apiTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace SqulioApi;
+
+use GuzzleHttp\Client;
+
+class Api
+{
+    protected $baseUrl;
+    protected $apiKey;
+
+    public function __construct($baseUrl, $apiKey)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->apiKey = $apiKey;
+    }
+
+    public function getUser()
+    {
+        $client = new Client([
+            'base_uri' => $this->baseUrl,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $this->apiKey,
+                'Accept' => 'application/json',
+            ]
+        ]);
+
+        $response = $client->get('/v1/users');
+
+        return json_decode($response->getBody(), true);
+    }
+}
